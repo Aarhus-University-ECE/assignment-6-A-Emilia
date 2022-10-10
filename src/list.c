d987b7523ef9ed47bf3398f8378bd02723de40b6
A-Emilia <CHKW@live.dk>
2022-10-10 20:05:05 +0200

    Assignment 6

--- src/list.c
+++ src/list.c
@@ -22,27 +22,48 @@ void add(node *head, int x){
 }
 
 int size(node *l){
-    // Excercise 3b)
-    // Add your code here... 
+  // Excercise 3b)
+  assert(l!=NULL);
 
-    return -1;
+  int c=0; // Counter
+  node *p=l->next; // Pointer to l so counting can start
+  
+  while(p!=NULL){
+    c++;
+    p=p->next;
+  }
+  printf("%d\n",c);
+  return c;
 }
 
 void printout(node *l) {
-  /*Excercise 3d) Implement your changes.. 
-    pre: head points to the first, empty element. The last element's next is NULL
-    post: the values of the list are printed out*/
-    node *p = l->next;
-    while (p!=NULL){
-      printf("%d, ",p->data);
-    }
-    printf("\n");
+/*pre: head points to the first, empty element.
+The last element’s next is NULL
+post: the values of the list are printed out*/
+  node *p = l->next;
+  while (p!=NULL){
+    printf("\%d, ",p->data);
+    p=p->next;
+  }
+printf("\n");
 }
 
 int largest(node *l){
-    /*Excercise 3e) Add your code below.
-      pre: head points to the first, empty element. The last element's next is NULL. size(l>0)
-      post: returns the largest value of the list*/
-    return -1; 
+/*Excercise 3e) Add your code below.
+pre: head points to the first, empty element. The last element's next is NULL. size(l>0)
+post: returns the largest value of the list*/
+  assert(l!=NULL);
+
+  node *p=l->next;
+  int big=p->data;
+
+  while(p!=NULL){
+    if(p->data>big){
+      big=p->data;
+    }
+    p=p->next;
+  }
+  printf("%d\n",big);
+  return big;
 }
 
--- src/max.c
+++ src/max.c
@@ -3,8 +3,13 @@
 
 int max(int* numbers, int size)
 {
-    // Excercise 2
-    // Implement your code below...
+  assert(size>0);
+  int max=numbers[0];
 
-    return -1;
+    for(int c=1;c<size;c++){
+      if(max<numbers[c]) {
+         max=numbers[c];
+         }
+      }
+    return max;
 }
